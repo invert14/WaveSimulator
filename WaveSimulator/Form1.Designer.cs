c80149541613f83bd61e145e80830562b9e76540
Kajetan <invert14@gmail.com>
2014-11-29 22:36:52 +0100

    first commit

---
+++ WaveSimulator/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace WaveSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dtTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.cdtTextBox = new System.Windows.Forms.TextBox();
+            this.cdxTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dxTextBox = new System.Windows.Forms.TextBox();
+            this.cStopTimeTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.stopTimeTextBox = new System.Windows.Forms.TextBox();
+            this.currentSpeedTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.speedTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(381, 288);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play/Pause";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.trackBar1.Location = new System.Drawing.Point(402, 257);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(390, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(548, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dtTextBox
+            // 
+            this.dtTextBox.Location = new System.Drawing.Point(501, 56);
+            this.dtTextBox.Name = "dtTextBox";
+            this.dtTextBox.Size = new System.Drawing.Size(60, 20);
+            this.dtTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(442, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Time step";
+            // 
+            // updateButton
+            // 
+            this.updateButton.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.updateButton.Location = new System.Drawing.Point(445, 205);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(78, 46);
+            this.updateButton.TabIndex = 6;
+            this.updateButton.Text = "Update values";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // cdtTextBox
+            // 
+            this.cdtTextBox.Enabled = false;
+            this.cdtTextBox.Location = new System.Drawing.Point(572, 56);
+            this.cdtTextBox.Name = "cdtTextBox";
+            this.cdtTextBox.Size = new System.Drawing.Size(60, 20);
+            this.cdtTextBox.TabIndex = 7;
+            // 
+            // cdxTextBox
+            // 
+            this.cdxTextBox.Enabled = false;
+            this.cdxTextBox.Location = new System.Drawing.Point(572, 82);
+            this.cdxTextBox.Name = "cdxTextBox";
+            this.cdxTextBox.Size = new System.Drawing.Size(60, 20);
+            this.cdxTextBox.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(430, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Spacial step";
+            // 
+            // dxTextBox
+            // 
+            this.dxTextBox.Location = new System.Drawing.Point(501, 82);
+            this.dxTextBox.Name = "dxTextBox";
+            this.dxTextBox.Size = new System.Drawing.Size(60, 20);
+            this.dxTextBox.TabIndex = 8;
+            // 
+            // cStopTimeTextBox
+            // 
+            this.cStopTimeTextBox.Enabled = false;
+            this.cStopTimeTextBox.Location = new System.Drawing.Point(572, 108);
+            this.cStopTimeTextBox.Name = "cStopTimeTextBox";
+            this.cStopTimeTextBox.Size = new System.Drawing.Size(60, 20);
+            this.cStopTimeTextBox.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(442, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Stop time";
+            // 
+            // stopTimeTextBox
+            // 
+            this.stopTimeTextBox.Location = new System.Drawing.Point(501, 108);
+            this.stopTimeTextBox.Name = "stopTimeTextBox";
+            this.stopTimeTextBox.Size = new System.Drawing.Size(60, 20);
+            this.stopTimeTextBox.TabIndex = 11;
+            // 
+            // currentSpeedTextBox
+            // 
+            this.currentSpeedTextBox.Enabled = false;
+            this.currentSpeedTextBox.Location = new System.Drawing.Point(572, 134);
+            this.currentSpeedTextBox.Name = "currentSpeedTextBox";
+            this.currentSpeedTextBox.Size = new System.Drawing.Size(60, 20);
+            this.currentSpeedTextBox.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(399, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Propagation speed";
+            // 
+            // speedTextBox
+            // 
+            this.speedTextBox.Location = new System.Drawing.Point(501, 134);
+            this.speedTextBox.Name = "speedTextBox";
+            this.speedTextBox.Size = new System.Drawing.Size(60, 20);
+            this.speedTextBox.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 315);
+            this.Controls.Add(this.currentSpeedTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.speedTextBox);
+            this.Controls.Add(this.cStopTimeTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.stopTimeTextBox);
+            this.Controls.Add(this.cdxTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dxTextBox);
+            this.Controls.Add(this.cdtTextBox);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox dtTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.TextBox cdtTextBox;
+        private System.Windows.Forms.TextBox cdxTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox dxTextBox;
+        private System.Windows.Forms.TextBox cStopTimeTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox stopTimeTextBox;
+        private System.Windows.Forms.TextBox currentSpeedTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox speedTextBox;
+    }
+}
+
---
+++ WaveSimulator/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace WaveSimulator
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private BackgroundWorker bw = new BackgroundWorker();
+
+        const int s = 128;
+        const int radius = s/16;
+        const double ratio = 8;
+        double[,] p = new double[s, s];
+        double[,] c = new double[s, s];
+        double[,] f = new double[s, s];
+        int speed = 10;
+        double dt = 0.00005;
+        double dx = 0.01;
+        double r;
+        double stopTime = 0.1;
+        int maxIter;
+        int rememberStep;
+//        int maxIter = 1;
+
+        private Dictionary<int, double[,]> mem = new Dictionary<int, double[,]>();
+        private bool simulationPaused = true;
+        private bool simulationStarted = false;
+        private ColorPalette colorPalette;
+        private Bitmap bitmap;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            g = panel1.CreateGraphics();
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            bw.DoWork += bw_DoWork;
+            bw.ProgressChanged += bw_ProgressChanged;
+            bw.RunWorkerCompleted += bw_RunWorkerCompleted;
+//            bw.RunWorkerAsync();
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = 100;
+            InitializeGrid();
+            InitializeBitmap();
+            UpdateSettings();
+        }
+
+        void UpdateSettings()
+        {
+            r = speed*dt/dx;
+            maxIter = (int) (stopTime/dt);
+            rememberStep = maxIter/100;
+            UpdateSettingsControls();
+        }
+
+        void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            simulationPaused = true;
+            SetSettingsControlsEnabled(true);
+        }
+
+        void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            trackBar1.Value = e.ProgressPercentage;
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+//            c[s / 2, s / 2] = -2.0;
+//            p[s / 2, s / 2] = -2.0;
+
+
+//            for (int i = s/2 - radius; i <= s/2 + radius; i++)
+//            {
+//                for (int j = s/2 - radius; j <= s/2 + radius; j++)
+//                {
+//                    double val = -(2*Math.Cos(Math.PI/(radius*2)*j)*Math.Cos(Math.PI/(radius*2)*i));
+//                    c[i, j] = val;
+//                    p[i, j] = c[i, j];
+//                }
+//            }
+
+            bool remember;
+            int rememberIter = 0;
+
+            for (int iter = 0; iter <= maxIter; iter++)
+            {
+                while (simulationPaused)
+                {
+                }
+
+                if (iter%rememberStep == 0)
+                    remember = true;
+                else
+                    remember = false;
+                bw.ReportProgress(100*iter/maxIter);
+
+                // fill in rgbValues, e.g. with a for loop over an input array
+
+                Random random = new Random();
+                for (int x = 1; x < s - 1; x++)
+                {
+                    for (int y = 1; y < s - 1; y++)
+                    {
+                        double val = r*r*(c[x - 1, y] - c[x, y]) +
+                                     r*r*(c[x + 1, y] - c[x, y]) +
+                                     r*r*(c[x, y - 1] - c[x, y]) +
+                                     r*r*(c[x, y + 1] - c[x, y]) +
+                                     2*c[x, y] - p[x, y];
+//                        double val = (2*c[x, y] - p[x, y] +
+//                                      r*r*(c[x - 1, y] + c[x + 1, y] + c[x, y - 1] + c[x, y + 1] - 4*c[x, y]));
+
+                        int pom = (int) (val*1000);
+                        f[x, y] = val;
+
+                        f[x, 0] = (2*c[x, 0] + (r - 1)*p[x, 0] +
+                            2*r*r*(c[x, 1] + c[x + 1, 0] + c[x - 1, 0] - 3*c[x, 0]))/(1 + r);
+                        f[x, s - 1] = (2*c[x, s - 1] + (r - 1)*p[x, s - 1] + 
+                            2*r*r*(c[x, s - 2] + c[x + 1, s - 1] + c[x - 1, s - 1] - 3*c[x, s - 1]))/(1 + r);
+                        f[0, y] = (2*c[0, y] + (r - 1)*p[0, y] +
+                            2*r*r*(c[1, y] + c[0, y + 1] + c[0, y - 1] - 3*c[0, y]))/(1 + r);
+                        f[s-1, y] = (2 * c[s-1, y] + (r - 1) * p[s-1, y] +
+                            2 * r * r * (c[s-2, y] + c[s-1, y + 1] + c[s-1, y - 1] - 3 * c[s-1, y])) / (1 + r);
+
+                        f[0, 0] = (2*c[0, 0] + (r - 1)*p[0, 0] + 
+                            2*r*r*(c[1, 0] + c[0, 1] - 2*c[0, 0]))/(1 + r);
+                        f[s - 1, s - 1] = (2*c[s - 1, s - 1] + (r - 1)*p[s - 1, s - 1] +
+                            2*r*r*(c[s - 2, s - 1] + c[s - 1, s - 2] - 2*c[s - 1, s - 1]))/(1 + r);
+                        f[0, s-1] = (2 * c[0, s-1] + (r - 1) * p[0, s-1] +
+                            2 * r * r * (c[1, s-1] + c[0, s-2] - 2 * c[0, s-1])) / (1 + r);
+                        f[s-1, 0] = (2 * c[s-1, 0] + (r - 1) * p[s-1, 0] +
+                            2 * r * r * (c[s-2, 0] + c[s-1, 1] - 2 * c[s-1, 0])) / (1 + r);
+
+//                        rgbValues[x*bmpData.Stride + y] = (byte) (c[x, y]/ratio*256);
+
+                        if (remember)
+                        {
+                            if (!mem.ContainsKey(rememberIter))
+                            {
+                                mem[rememberIter] = new double[s, s];
+                            }
+                            mem[rememberIter][x, y] = c[x, y];
+                        }
+                        p[x, y] = c[x, y];
+                        c[x, y] = f[x, y];
+                    }
+                }
+                if (remember)
+                    rememberIter++;
+                
+                UpdateBitmap(c);
+                
+                Console.Out.WriteLine(c[s/2,s/2]);
+            }
+        }
+
+        private void InitializeGrid()
+        {
+            for (int i = 0; i != s; i++)
+            {
+                for (int j = 0; j != s; j++)
+                {
+                    c[i, j] = 0d;
+                    p[i, j] = 0d;
+                    f[i, j] = 0d;
+                }
+            }
+        }
+
+        private void UpdateBitmap(double[,] values)
+        {
+            var BoundsRect = new Rectangle(0, 0, s, s);
+            BitmapData bmpData = bitmap.LockBits(BoundsRect,
+                ImageLockMode.WriteOnly,
+                bitmap.PixelFormat);
+            IntPtr ptr = bmpData.Scan0;
+            int length = bmpData.Stride * bitmap.Height;
+            var rgbValues = new byte[length];
+            for (int x = 0; x <= s - 1; x ++)
+            {
+                for (int y = 0; y <= s - 1; y ++)
+                {
+                    rgbValues[x * bmpData.Stride + y] = (byte)(values[x, y] / ratio * 256 + 127);
+                }
+            }
+            Marshal.Copy(rgbValues, 0, ptr, length);
+            bitmap.UnlockBits(bmpData);
+            g.DrawImage(bitmap, 0, 0);
+        }
+
+        private void InitializeBitmap()
+        {
+            bitmap = new Bitmap(s, s, PixelFormat.Format8bppIndexed);
+            colorPalette = bitmap.Palette;
+            for (int i = 0; i < 256; i++)
+                colorPalette.Entries[i] = Color.FromArgb(255, i, i, i);
+            bitmap.Palette = colorPalette;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!bw.IsBusy)
+            {
+                UpdateSettings();
+                bw.RunWorkerAsync();
+                SetSettingsControlsEnabled(false);
+            }
+
+            if (!simulationPaused)
+                simulationPaused = true;
+            else
+                simulationPaused = false;
+        }
+
+        private void SetSettingsControlsEnabled(bool state)
+        {
+            updateButton.Enabled = state;
+
+            label1.Enabled = state;
+            dtTextBox.Enabled = state;
+            label2.Enabled = state;
+            dxTextBox.Enabled = state;
+            label3.Enabled = state;
+            stopTimeTextBox.Enabled = state;
+            label4.Enabled = state;
+            speedTextBox.Enabled = state;
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            int val = trackBar1.Value;
+            if (simulationPaused && mem.ContainsKey(val))
+            {
+                UpdateBitmap(mem[val]);
+                Console.Out.WriteLine(trackBar1.Value);
+            }
+        }
+
+        private void StartWave(int cx, int cy)
+        {
+            int radius = 2;
+            for (int x = cx - radius; x <= cx + radius; x++)
+            {
+                for (int y = cy - radius; y <= cy + radius; y++)
+                {
+                    if (IsPointInBounds(x, y, s, s))
+                    {
+                        c[x, y] = -2.0;
+                        p[x, y] = -2.0;
+                    }
+                }
+            }
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Console.Out.WriteLine(e.X + " " + e.Y);
+            if (IsPointInBounds(e.X, e.Y, s, s))
+            {
+                StartWave(e.Y, e.X);
+            }
+        }
+
+        private bool IsPointInBounds(int x, int y, int xBound, int yBound)
+        {
+            return (x > 0 && x < xBound - 1 && y > 0 && y < yBound - 1);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            UpdateBitmap(c);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InitializeGrid();
+            mem.Clear();
+            UpdateBitmap(c);
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dtTextBox.Text.Length > 0)
+                {
+                    dt = Convert.ToSingle(dtTextBox.Text, CultureInfo.InvariantCulture);
+                }
+                if (dxTextBox.Text.Length > 0)
+                {
+                    dx = Convert.ToSingle(dxTextBox.Text, CultureInfo.InvariantCulture);
+                }
+                if (stopTimeTextBox.Text.Length > 0)
+                {
+                    stopTime = Convert.ToSingle(stopTimeTextBox.Text, CultureInfo.InvariantCulture);
+                }
+                if (speedTextBox.Text.Length > 0)
+                {
+                    speed = Convert.ToInt32(speedTextBox.Text, CultureInfo.InvariantCulture);
+                }
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Unable to convert!");
+            }
+            UpdateSettingsControls();
+        }
+
+        private void UpdateSettingsControls()
+        {
+            dtTextBox.Clear();
+            dxTextBox.Clear();
+            stopTimeTextBox.Clear();
+            speedTextBox.Clear();
+
+            cdtTextBox.Text = dt.ToString("F6");
+            cdxTextBox.Text = dx.ToString("F3");
+            cStopTimeTextBox.Text = stopTime.ToString("F3");
+            currentSpeedTextBox.Text = speed.ToString();
+        }
+    }
+}
